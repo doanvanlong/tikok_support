957ac639b13bd9fbf2d82d6e915b81e4abf421e5
Đoàn Văn Long <47972513+doanvanlong@users.noreply.github.com>
2024-04-29 10:58:31 +0700

    s

--- src/pages/not-found.vue
+++ src/pages/not-found.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable max-len -->
 <template>
   <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
@@ -103,7 +104,54 @@
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
       </svg>
     </button>
-</div>
+  </div>
+  <div v-if="erorrCors" id="YOUR_ID" class="fixed z-50 inset-0 overflow-y-auto">
+      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+              <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+          </div>
+          <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+          <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
+              role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+              <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                  <button @click="erorrCors= false" type="button" data-behavior="cancel" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                      <span class="sr-only">Close</span>
+                      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                      </svg>
+                  </button>
+              </div>
+              <div class="sm:flex sm:items-start">
+                  <div
+                      class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <svg class="h-6 w-6 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+                          viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                      </svg>
+                  </div>
+                  <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                          Currently the Proxy server has failed
+                      </h3>
+                      <div class="mt-2">
+                          <p class="text-sm text-gray-500">
+                              Please click accept to authenticate the proxy server.
+                          </p>
+                      </div>
+                  </div>
+              </div>
+              <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                  <button type="button" @click="openNewTab(urlRedirect)" data-behavior="commit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
+                      Accept
+                  </button>
+                  <button @click="erorrCors= false" type="button" data-behavior="cancel" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:w-auto sm:text-sm">
+                      Cancel
+                  </button>
+              </div>
+          </div>
+      </div>
+  </div>
   <div class="container m-auto pt-3">
     <template v-if="loading" >
       <div class="text-center mb-3">
@@ -190,7 +238,6 @@
 import CryptoJS from 'crypto-js';
 import moment from 'moment';
 import 'moment-timezone';
-// USLC9UELE7&ids=
 // eslint-disable-next-line no-unused-vars
 export default {
   data() {
@@ -216,6 +263,8 @@ export default {
       k2: 0,
       orderDetailAdmin: '',
       show: false,
+      erorrCors: false,
+      urlRedirect: '',
     };
   },
   created() {
@@ -224,6 +273,12 @@ export default {
   },
   methods: {
     // eslint-disable-next-line no-unused-vars
+    openNewTab(_url) {
+      setTimeout(() => {
+        this.fetchShopData();
+      }, 10000);
+      window.open(_url, '_blank');
+    },
     callRequest(_url) {
       if (_url) {
         this.loading = true;
@@ -304,6 +359,11 @@ export default {
         .catch((error) => {
           // eslint-disable-next-line no-console
           console.log('Error:', error);
+          if (error.response.data.includes('See /corsdemo for more info')) {
+            this.erorrCors = true;
+            this.urlRedirect = proxyUrl + apiUrl;
+            // this.openNewTab(this.urlRedirect);
+          }
           throw error;
         });
     },
@@ -414,6 +474,7 @@ export default {
           ).then((orderListt) => {
             // eslint-disable-next-line no-console
             console.log(orderListt); // Log kết quả của getOrderListTiktok()
+
             // const listOrder = orderList.data.order_list.map((v) => v.order_id);
             this.loading = false;
             this.orderList = [];
